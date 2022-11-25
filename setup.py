37fa00b5e52cdf2f1d3d32272f40ee53dff94dca
dev-jam <mail@bobrosbag.nl>
2022-11-25 16:29:53 +0100

    fix format

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     version='2.3.0',
     description='An OpenSesame Plug-in for collecting button responses, audio detection, voice key and sending stimulus synchronization triggers with the Radboud Buttonbox to data acquisition systems.',
     long_description=get_readme(),
+    long_description_content_type='text/markdown',
     author='Bob Rosbag',
     author_email='debian@bobrosbag.nl',
     url='https://github.com/dev-jam/opensesame-plugin-radboudbox',

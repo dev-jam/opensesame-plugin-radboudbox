3a5e270c234ec696ba16d3b4d7a9b64bf9aba9fe
dev-jam <mail@bobrosbag.nl>
2025-07-18 11:17:01 +0200

    bump version

--- opensesame_plugins/radboudbox/radboudbox_get_buttons_wait/__init__.py
+++ opensesame_plugins/radboudbox/radboudbox_get_buttons_wait/__init__.py
@@ -9,6 +9,6 @@ controls = [
         "label": "<small><b>Note:</b> Radboudbox init item at the begin of the experiment is needed for initialization of the buttonbox</small>"
     }, {
         "type": "text",
-        "label": "<small>Radboud Buttonbox version 4.1.1</small>"
+        "label": "<small>Radboud Buttonbox version 4.2.0</small>"
     }
 ]
